d89b3ad25c3585aa04106bd9a1083e78c5d6e213
Jayaram-p-7 <57484379+Jayaram-p-7@users.noreply.github.com>
2019-12-26 19:29:42 +0530

    Add files via upload

---
+++ si.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+void product(vector<int> from,vector<int> to,vector<int> w) {
+    map<int,set<int>> m;
+    for(int i=0;i<to.size();i++) {
+    m[w[i]].insert(from[i]);
+    m[w[i]].insert(to[i]);
+    }
+    map<pair<int,int>,int> p;
+    int max=INT_MIN;
+    int max_product=INT_MIN;
+    for(auto it=m.begin();it!=m.end();it++){
+        set<int> s=it->second;
+        vector<int> v(s.begin(),s.end());
+        int siz=v.size();
+        for(int i=0;i<siz-1;i++) {p[make_pair(v[i],v[i+1])]++;
+        if(max<p[make_pair(v[i],v[i+1])]) max=p[make_pair(v[i],v[i+1])];
+        }
+        if(siz>2) {
+        p[make_pair(v[0],v[siz-1])]++;
+        if(max<p[make_pair(v[0],v[siz-1])]) max=p[make_pair(v[0],v[siz-1])];}
+    }
+    for(auto itr=p.begin();itr!=p.end();itr++)
+    {
+        pair<int,int> pr=itr->first;
+        if(itr->second==max)
+            if(max_product<pr.first*pr.second) max_product=pr.first*pr.second;
+
+    }
+    cout<<max_product;
+}
+
+int main() {
+
+int n=4,e=5;
+
+vector<int> from = {1, 1, 2, 2, 2};
+vector<int> to =    {2, 2, 3, 3, 4};
+vector<int> wt = {2, 3, 1, 3, 4 };
+
+product(from,to,wt);
+}
